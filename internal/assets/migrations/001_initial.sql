be445b2173eda7cd61ac61aecee75de1071662a2
Denis Denysyev <dvdenysyev@gmail.com>
2022-09-07 15:12:51 +0200

    Change fields

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,9 +1,18 @@
 -- +migrate Up
 create table blobs (
     id SERIAL PRIMARY KEY,
-    owner_id TEXT,
+    owner_address Bytea,
     purpose TEXT,
     blob_content json not null
 );
+create table documents(
+    id SERIAL PRIMARY KEY,
+    type TEXT,
+    image Bytea,
+    format_file TEXT,
+    document_name TEXT,
+    owner_address BYTEA
+);
 -- +migrate Down
 drop table blobs;
+drop table documents;
